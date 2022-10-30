85529830d90966ec648d6f6b05bdaee457a382b1
Kotikov <11deadsluts@gmail.com>
2022-10-31 01:35:07 +0300

    three tasks is solved

--- extraHW.cs
+++ extraHW.cs
@@ -54,22 +54,46 @@ AngleTime();
 //Найти число x. Значение n вводится с клавиатуры, 100 ≤ n ≤ 999 
 //и при этом число десятков в n не равно нулю.
 
-int whatAboutX(int n)
+// int whatAboutX(int n)
+// {
+//     if (n<109 || n>999)
+//     {
+//         System.Console.WriteLine("Please try again and input correct number");
+//     }
+//     else
+//     {
+//         int a = n % 100;
+//         int b = n / 100;
+//         int x = a * 10 + b;
+//         System.Console.WriteLine($"(n) was {n} and (x) is {x}");
+//     }
+//     return n;
+// }
+
+// System.Console.WriteLine("pls, input any (n)umber from 110 to 999 and you will find out (x)");
+// int n = Convert.ToInt32(Console.ReadLine());
+// n = whatAboutX(n);
+
+// В трехзначном числе x зачеркнули первую цифру. 
+// Когда полученное число умножили на 10, а произведение сложили с первой цифрой числа x,
+// то получилось число n. По заданному n найти число y (значение n вводится с клавиатуры, 1 ≤ n ≤ 999).
+
+int whatAboutY(int nn)
 {
-    if (n<109 || n>999)
+    if (nn < 109 || nn > 999)
     {
         System.Console.WriteLine("Please try again and input correct number");
     }
     else
     {
-        int a = n % 100;
-        int b = n / 100;
-        int x = a * 10 + b;
-        System.Console.WriteLine($"(n) was {n} and (x) is {x}");
+        int a = nn % 10;
+        int b = nn / 10;
+        int y = a * 100 + b;
+        System.Console.WriteLine($"(n) was {nn} and (x) is {y}");
     }
-    return n;
+    return nn;
 }
 
-System.Console.WriteLine("pls, input any number from 110 to 999");
-int n = Convert.ToInt32(Console.ReadLine());
-n = whatAboutX(n);
+System.Console.WriteLine("pls, input any (n)umber from 110 to 999 and you will find out (y)");
+int nn = Convert.ToInt32(Console.ReadLine());
+nn = whatAboutY(nn);
